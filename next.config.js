48470a027bf7fa6cfe1a689565ddd7b598ac0baa
Conradbubex <122864099+Conradbubex@users.noreply.github.com>
2025-10-28 00:15:32 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+/** @type {import('next').NextConfig} */
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
@@ -7,6 +8,7 @@ const nextConfig = {
     unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "direct2kariakoo.co.tz", pathname: "/**" },
+      { protocol: "https", hostname: "direct2kariakoo.com", pathname: "/**" },
       { protocol: "http", hostname: "127.0.0.1", port: "8000", pathname: "/**" },
       { protocol: "http", hostname: "localhost", port: "8000", pathname: "/**" },
     ],
@@ -14,7 +16,12 @@ const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
   trailingSlash: true,
+
+  // ✅ Important for Vercel static build
   output: "export",
+  distDir: "out",
+
+  // ✅ Prevents build crash due to console logs
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
